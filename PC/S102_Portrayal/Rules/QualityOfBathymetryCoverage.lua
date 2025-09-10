3ac06c0eaef5c36169e84cacf3c2f1f90b6d7bf5
Rian-Admin <rnd@rian.kr>
2025-09-11 08:17:33 +0900

    ProgramData.zip으로 변경

--- PC/S102_Portrayal/Rules/QualityOfBathymetryCoverage.lua
+++
@@ -1,5 +0,0 @@
-function QualityOfBathymetryCoverage(feature, featurePortrayal, contextParameters)
-	
-	featurePortrayal:AddInstructions('ViewingGroup:13030;DrawingPriority:3;DisplayPlane:UnderRadar;NullInstruction')
-
-end
